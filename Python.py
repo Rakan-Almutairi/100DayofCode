2247b70bb86cadaccd27dec137f27aa5899db90c
Rakan EW <44053941+Rakan-Almutiri@users.noreply.github.com>
2019-08-21 13:38:41 +0300

    done lesson 5

--- Python.py
+++ Python.py
@@ -1,7 +1,22 @@
-import time
-if 1>0:
-    print("Hello,World")
-time.sleep(10) #pasue to 10 sec
+import time,random
+
+timesleep =600
+
+print("Hello,World")
 x,y,z ='Rakan' ,'Eid' ,'Almutairi'
-print(x,y,z)
-time.sleep(10) #pasue to 10 sec
+print("\n",x,y,z)
+
+#random number
+a =random.randrange(0,10)
+print("\nRandom number range 0-10 =>",a)
+
+#varibales and changes type
+x1 =10
+x2 =10.15
+x3 =10j
+print("\n\tx1",x1,type(x1),"\n\tx2",x2,type(x2),"\n\tx3",x3,type(x3),"\nChange type")
+x1=float(x1)
+x2=int(x2)
+print("\tx1",x1,type(x1),"\n\tx2",x2,type(x2),"\n\tx3",x3,type(x3),"====>Can't change the type xD")
+print("\nsleep time",timesleep)
+time.sleep(timesleep) #pasue to 600 sec 
