5ad9eb36c3ec8dfb7255753aab88468c44d6e15d
Rakan Almutairi <44053941+Rakan-Almutiri@users.noreply.github.com>
2019-09-01 11:08:02 +0300

    change the comment

--- Python.py
+++ Python.py
@@ -21,25 +21,25 @@ def SecoundWeek():
     #string practise 
     print(a) #print multi line 
     a="Hello,World"
-    print(a[11:41]) #print range from 11 to 41 
-    print(a.strip()) #eraise whitespace in start of string
-    print(len(a)) #print length of the string
-    print(a.upper(),"\t",a.lower()) #uppper alph ,lower
-    print(a.replace("Hello,World","Hi there")) #replce 
-    print(a.split(',')) #split by ","
+    print(a[1:4]+"\t#print range from 1 to 4") 
+    print(a.strip()+"\t#eraise whitespace in start of string")
+    print(len(a),"\t#print length of the string") 
+    print(a.upper(),"\t",a.lower()+"\t#uppper alph ,lower") 
+    print(a.replace("Hello,World","Hi there")+"\t#replce") 
+    print(a.split(','),"#\tsplit by ','") 
     a="Hello,World {}"
     x=20
-    print(a.format(x)) #replace {} by x 
+    print(a.format(x)+"\t#replace {} by x")  
     a="Hello{0}World\t{1}"
-    print(a.format(',',20)) #multi replace format
+    print(a.format(',',20)+"\t#multi replace format") 
 def Oprations():
     x,y =3,12
     a=["Rakan","Khaled"]
     print(x is not y)
     print(x // y)
-    print("Rakan" in a) #search Rakan is it in a list
+    print("Rakan" in a,"\t#search Rakan is it in a list") 
 
 #lessons fun 
-#FirstWeek()
+FirstWeek()
 SecoundWeek()
-#Oprations()
+Oprations()
